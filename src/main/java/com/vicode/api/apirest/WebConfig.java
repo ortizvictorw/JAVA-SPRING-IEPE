3f9d86bd816f75cd56bcdbbc3b3b1c16efefe2a8
@vortiz-agropago <vortiz@agropago.com>
2024-04-18 14:04:22 -0300

    agrego cors

---
+++ src/main/java/com/vicode/api/apirest/WebConfig.java
@@ -0,0 +1,19 @@
+package com.vicode.api.apirest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @SuppressWarnings("null")
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") // Permitir solicitudes desde cualquier origen
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Permitir los métodos especificados
+                .allowCredentials(true); // Permitir el uso de credenciales, si es necesario
+    }
+}
